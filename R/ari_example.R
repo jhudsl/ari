2bbeb9beb316909c478c05f3a9d074392d4cead6
seankross <smk240@gmail.com>
2017-05-18 15:46:57 +0000

    added ari_example

---
+++ R/ari_example.R
@@ -0,0 +1,17 @@
+#' Get the path to an ari example file
+#'
+#' This function allows you to quickly access files that are used in
+#' the ari documentation.
+#'
+#' @param path The name of the file. If no argument is provided then
+#' all of the example files will be listed.
+#' @export
+#' @examples
+#' ari_example("ari_intro.Rmd")
+ari_example <- function(path = NULL){
+  if(is.null(path)) {
+    list.files(system.file("test", package = "ari"))
+  } else {
+    system.file("test", path, package = "ari", mustWork = TRUE)
+  }
+}
