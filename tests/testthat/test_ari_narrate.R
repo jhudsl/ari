b5c0f287ccaf3c8d270f411ef84fdd8b8e54b67a
seankross <smk240@gmail.com>
2017-08-30 15:26:41 -0400

    updated travis and tests

--- tests/testthat/test_ari_narrate.R
+++ tests/testthat/test_ari_narrate.R
@@ -11,12 +11,11 @@ video <- file.path(tempdir(), "output.mp4")
 
 test_that("Ari can make a video from local HTML slides.", {
   skip_on_cran()
-  skip_on_travis()
   skip_narrate()
   
   ari_narrate(system.file("test", "ari_intro_script.md", package = "ari"),
               system.file("test", "ari_intro.html", package = "ari"),
-              video, voice = list_voices()$Id[1])
+              video, voice = list_voices()$Id[1], capture_method = "iterative")
   expect_true(file.size(video) > 50000)
 })
 
@@ -24,12 +23,11 @@ unlink(video, force = TRUE)
 
 test_that("Ari can make a video from HTML slides on the web.", {
   skip_on_cran()
-  skip_on_travis()
   skip_narrate()
   
   ari_narrate(system.file("test", "ari_intro_script.md", package = "ari"),
               "http://seankross.com/ari/inst/test/ari_intro.html",
-              video, voice = list_voices()$Id[1])
+              video, voice = list_voices()$Id[1], capture_method = "iterative")
   expect_true(file.size(video) > 50000)
 })
 
@@ -37,7 +35,6 @@ unlink(video, force = TRUE)
 
 test_that("Ari use an Rmd file with HTML comments for a script.", {
   skip_on_cran()
-  skip_on_travis()
   skip_narrate()
   
   ari_narrate(system.file("test", "ari_comments.Rmd", package = "ari"),
