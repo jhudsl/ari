9db9483a8a99600d41a62bedcdcc8995cbb35ec4
muschellij2 <muschellij2@gmail.com>
2018-11-29 17:42:13 -0500

    as per https://www.proz.com/forum/subtitling/322102-controlling_the_subtitle_length_in_aegisub.html https://partnerhelp.netflixstudios.com/hc/en-us/articles/215274938-What-is-the-maximum-number-of-characters-per-line-allowed-in-Timed-Text-assets- http://www.channel4.com/media/documents/corporate/foi-docs/SG_FLP.pdf, it seems as though 42 is a limit

--- R/ari_subtitles.R
+++ R/ari_subtitles.R
@@ -3,11 +3,11 @@
 # path - path to .srt file output
 #' @importFrom purrr map map_dbl map2
 #' @importFrom hms hms
-ari_subtitles <- function(paragraphs, wavs, path) {
+ari_subtitles <- function(paragraphs, wavs, path, width = 42) {
   durations <- map_dbl(wavs, ~ length(.x@left) / .x@samp.rate)
   
   # Break down paragraphs so that they are more readable
-  paragraphs <- map(paragraphs, strwrap, width = 60)
+  paragraphs <- map(paragraphs, strwrap, width = width)
   lines_per_paragraph <- map_dbl(paragraphs, length)
   durations <- rep(durations / lines_per_paragraph, times = lines_per_paragraph)
   paragraphs <- unlist(paragraphs)
@@ -20,8 +20,14 @@ ari_subtitles <- function(paragraphs, wavs, path) {
   # need to check to see if no , so 00:00:27 doesn't happen
   cumdur = unlist(cumdur)
   add = !grepl(",", cumdur)
-  cumdur[ add ] = paste0(cumdur[add], ",0")
+  cumdur[ add ] = paste0(cumdur[add], ",000")
   cumdur <- c("00:00:00,000", unlist(cumdur))
+  cumdur = strsplit(cumdur, ",")
+  cumdur <- map_chr(cumdur, function(x) {
+    x[2] = sprintf("%03.0f", as.numeric(x[2]))
+    x = paste(x, collapse = ",")
+  })
+  
   
   result <- seq_along(paragraphs)
   result <- map(result, ~ c(.x, paste(cumdur[.x], "-->", cumdur[.x + 1]), 
