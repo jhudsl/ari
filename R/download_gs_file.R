d80cb3b63d3542ad8400120199af9cfd7e3f91e2
howardbaek <howardbaek.fh@gmail.com>
2023-06-02 15:01:08 -0700

    Add `download_gs_file()` and related functions from ariExtra

--- R/download_gs_file.R
+++ R/download_gs_file.R
@@ -79,3 +79,115 @@ export_url <- function(id, page_id = NULL, type = "png") {
   }
   url
 }
+
+# Constructs an URL to export to pptx
+pptx_url = function(id) {
+  export_url(id, page_id = NULL, type = "pptx")
+}
+
+# Constructs an URL to export to pdf
+pdf_url = function(id) {
+  export_url(id, page_id = NULL, type = "pdf")
+}
+
+
+
+# Extract page IDs of slides in a Google Slides presentation
+#' @importFrom jsonlite fromJSON
+get_page_ids = function(id) {
+  id = get_slide_id(id)
+  url = paste0("https://docs.google.com/presentation/d/", id)
+  tfile = tempfile(fileext = ".html")
+  res = httr::GET(url, httr::write_disk(tfile))
+  httr::stop_for_status(res)
+  cr = httr::content(res)
+  script = rvest::html_nodes(cr, xpath ="//script")
+  script = rvest::html_text(script)
+  script = unique(script)
+  script = gsub("DOCS_modelChunk = undefined;", "", script)
+  script = script[ grepl("DOCS_modelChunk\\s=\\s\\[", x = script)]
+
+  all_types = c("PREDEFINED_LAYOUT_UNSPECIFIED",
+                "BLANK",
+                "CAPTION_ONLY",
+                "TITLE",
+                "TITLE_AND_BODY",
+                "TITLE_AND_TWO_COLUMNS",
+                "TITLE_ONLY",
+                "SECTION_HEADER",
+                "SECTION_TITLE_AND_DESCRIPTION",
+                "ONE_COLUMN_TEXT",
+                "MAIN_POINT",
+                "BIG_NUMBER",
+                paste0("CUSTOM_", 1:100))
+  types = paste0(all_types, collapse = "|")
+  # script = script[grepl(types, script)]
+  ss = strsplit(script, "; DOC")
+  ss = lapply(ss, trimws)
+  ss = lapply(ss, function(x) {
+    x[!grepl("^DOC", x)] = paste0(" DOC", x[!grepl("^DOC", x)])
+    x
+  })
+  ss = lapply(ss, function(x) {
+    x = x[grepl("^DOCS_modelChunk\\s=\\s\\[", x)]
+    x = x[ !x %in% "DOCS_modelChunk = undefined"]
+    x = sub("^DOCS_modelChunk\\s=\\s\\[", "[", x)
+    x
+  })
+  ss = unlist(ss)
+  pages = lapply(ss, jsonlite::fromJSON)
+  pages = sapply(pages, function(x) {
+    x = x[sapply(x, function(r) any(unlist(r) %in% all_types))]
+    x = x[length(x)]
+    x
+  })
+  pages = sapply(pages, function(x) {
+    if (length(x) < 2) {
+      if (length(x) == 0) {
+        return(NA)
+      }
+      x = x[[1]]
+      if (length(x) < 2) {
+        return(NA)
+      }
+    }
+    x[[2]]
+  })
+  pages = pages[ !is.na(pages) ]
+  if (length(pages) >= 2) {
+    pages = c(pages[1], grep("^g", pages[2:length(pages)], value = TRUE))
+  }
+  if (pages[1] != "p") {
+    pages = unique(c("p", pages))
+  }
+  urls = export_url(id = id, page_id = pages)
+  pages = pages[check_png_urls(urls)]
+  pages
+}
+
+#' @rdname get_slide_id
+#' @export
+#' @examples
+#' x = "https://drive.google.com/drive/folders/1pXBQQdd1peI56GtQT-jEZ59xSmhqQlFC?usp=sharing"
+#' get_folder_id(x)
+#' x = "1pXBQQdd1peI56GtQT-jEZ59xSmhqQlFC"
+#' get_folder_id(x)
+get_folder_id = function(x) {
+  res = httr::parse_url(x)
+  x = res$path
+  x = sub(".*folders/", "", x)
+  x = sub("[?].*", "", x)
+  x = x[ nchar(x) > 5]
+  x = trimws(x)
+  x
+}
+
+#' @export
+#' @rdname get_slide_id
+make_slide_url <- function(x) {
+  x = get_slide_id(x)
+  x = paste0("https://docs.google.com/presentation/d/",x)
+  x
+}
+
+
